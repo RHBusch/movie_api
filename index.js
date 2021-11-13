cfc561e4a14f5220a9674fce74b9c0536ff187c5
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-11-13 16:32:07 -0500

    Updating index.js with relevant app.get language

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express'),
+    morgan = require('morgan');
+
+
+const app = express ();
+
+let topMovies = [
+    {
+      title: 'The Big Sick'
+    },
+
+    {
+        title: 'Forrest Gump'  
+    },
+
+    {
+        title: 'Lord of the Rings'  
+    },
+
+    {
+        title: 'Slumdog Millionaire'  
+    },
+
+    {
+        title: 'Inside Out'  
+    },
+
+    {
+        title: 'Despicable Me'  
+    },
+
+    {
+        title: 'War Horse'  
+    },
+
+    {
+        title: 'Dreamgirls'  
+    },
+
+    {
+        title: 'Les Miserables'  
+    },
+
+    {
+        title: 'Step Brothers'  
+    },
+]
+
+app.get('/movies',(req,res)=>{
+res.json(topMovies)
+});
+
+app.get('/',(req,res)=>{
+    res.send('What would Ted Lasso do?')
+    });
+
+app.use (morgan('common'));
+app.use (express.static('public'));
+
+app.use ((err,req,res,next) => {
+    console.error (err.stack);
+    res.status (500).send('Something broke!');
+});
+
+app.listen(8080, () =>{
+    console.log('This app is listening on port 8080.');
+  });
