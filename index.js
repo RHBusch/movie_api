3c99adb0014c20e5d9300afa6c9ccb87bc5e278d
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-11-17 19:40:48 -0500

    Writing the Express code to route client requests

--- index.js
+++ index.js
@@ -47,15 +47,6 @@ let topMovies = [
     },
 ]
 
-//Routing requests via /movies to the top movies json. 
-app.get('/movies',(req,res)=>{
-res.json(topMovies)
-});
-
-app.get('/',(req,res)=>{
-    res.send('What would Ted Lasso do?')
-    }); // Creating endpoint / with textual response. 
-
 app.use (morgan('common')); //Using morgan middleware to log requests. 
 app.use (express.static('public')); // Routing static file requests to the public folder. 
 
@@ -69,3 +60,61 @@ app.use ((err,req,res,next) => {
 app.listen(8080, () =>{
     console.log('Hey! FYI... This app is listening on port 8080...');
   });
+
+//Routing requests via /movies to the top movies json. 
+app.get('/movies',(req,res)=>{
+    res.json(topMovies)
+    });
+   
+//Routing requests for a single movie. 
+
+  app.get('/movies/:title',(req,res) =>{
+      res.send('Successful GET request returning data on the requested movie title.')
+  });
+
+//Routing requests for movies of a specific genre.
+
+  app.get('/movies/:genre',(req,res) =>{
+      res.send('Successful GET request returning data included in the requested movie genre.')
+  });
+
+//Routing reqeusts for movie data based on a single director. 
+
+  app.get('/movies/:director',(req,res) => {
+      res.send('Successful GET request returning data based on the requested director.')
+  });
+
+  //Routing the new user registration request 
+
+  app.post('/users',(req,res) => {
+      res.send('Successful POST request creating a new user and adding their data.')
+  });
+
+  //Routing the update username request
+
+  app.put('/users/:username',(req,res) => {
+      res.send ('Successful PUT request updating a user ID.')
+  });
+
+  //Routing request to add a movie to a user's list of favorites.
+
+  app.post('/users/add/:movieTitle', (req,res) =>{
+      res.send('Successful POST request adding a movie to user\'s list of favorites.')
+  });
+
+  //Routing request to delete a movie from a user's list of favorites. 
+
+  app.delete('/users/remove/:movieTitle', (req,res)=>{
+      res.send('Successful DELETE request removing a movie from a user\'s list of favorites')
+  });
+
+  //Routing a request to delete a user from the database. 
+  
+  app.delete('/users/remove/:username',(req,res) =>{
+      res.send ('Successful DELETE request removing a user from the database.')
+  });
+
+
+    app.get('/',(req,res)=>{
+        res.send('What would Ted Lasso do?')
+        }); // Creating endpoint / with textual response. 
