f32adc6671876f1c3889736cb218705d83b5d66e
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-11-24 10:02:40 -0500

    Adding code to SQL practice

--- practice.sql
+++ practice.sql
@@ -20,3 +20,19 @@ INSERT INTO Movies(Title, Description, GenreID, DirectorID,ImageUrl,Featured)
 VALUES('Silence of the Lambs','A young FBI cadet must receive the help of an 
 incarcerated and manipulative cannibal killer to help catch another 
 serial killer.',1,1,'silenceofthelambs.png',True);
+
+SELECT column_1, column_2 … column_n
+  FROM table_name
+  WHERE conditions;
+
+SELECT Title, Description, DirectorID
+  FROM Movies
+  WHERE GenreID = 1;
+
+SELECT *
+  FROM Movies
+  WHERE Title = 'Silence of the Lambs';
+
+SELECT Title, Description, DirectorID
+  FROM Movies
+  WHERE GenreID = 1 AND DirectorID = 1;
