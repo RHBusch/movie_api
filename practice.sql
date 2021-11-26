926ddf971816fa54703fb2b2a1a3728d679d355d
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-11-26 11:48:21 -0500

    Adding MongoDB practice queries

--- practice.sql
+++ practice.sql
@@ -36,3 +36,44 @@ SELECT *
 SELECT Title, Description, DirectorID
   FROM Movies
   WHERE GenreID = 1 AND DirectorID = 1;
+
+
+
+
+  var movie1 = {
+Title: "Lord of the Rings and the Two Towers",
+Description: "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
+ Genre:{
+  Name: "Fantasty",
+  Description: "Fantasy film is a genre that incorporates imaginative and fantastic themes. These themes usually involve magic, supernatural events, or fantasy worlds."
+  },
+    Ratings: {
+      IMDB: "8.7/10",
+      Rotten Tomatoes: "95%"
+       },
+  Director:{
+    Name: "Peter Jackson",
+   Bio: "Peter Jackson is a New Zealand film director, screenwriter, and film producer.",
+    Birth:"1961",
+  Death: "N/A"
+    },
+ ImagePath:"twotowers.png",
+   Featured: true}
+
+
+   {
+  Title: "Silence of the Lambs",
+  Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
+  Genre: {
+    Name: "Thriller",
+    Description: "Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience."
+  },
+  Director: {
+    Name: "Jonathan Demme",
+    Bio: "Robert Jonathan Demme was an American director, producer, and screenwriter.",
+    Birth: "1944",
+    Death: "2017"
+  },
+  ImagePath: "silenceofthelambs.png",
+  Featured: true
+}
