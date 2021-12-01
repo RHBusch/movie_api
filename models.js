7817922c4aa29b24e228ba141bf646cdd7692a21
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-11-30 20:28:49 -0500

    Creating a mongoose model

--- models.js
+++ models.js
@@ -1,2 +1,42 @@
 // This is where the Mongoose models will live. //
 const mongoose = require('mongoose');
+
+//Establishing schema for the movie database.
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required:true},
+    Description: {type: String, required:true},
+    Genre:{
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String, 
+        Bio: String
+    },
+    Ratings: {
+        IMDB: String,
+        RottenTomatoes: String 
+    },
+    Actors: [String],
+    ImagePath: String, 
+    Featured: Boolean
+});
+
+//Establishing schema for the users database.
+
+let userSchema = mongoose.Schema({
+    Username:{type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date, 
+    FavoriteMovies: 
+    [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
