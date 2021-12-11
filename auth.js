b0d7cbf6e3c8d0107b84619e03d5e2f2e9f8c725
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-12-11 16:04:26 -0500

    Writing code to generate a JWT Token for successful logins

--- auth.js
+++ auth.js
@@ -26,10 +26,9 @@ module.exports = (router) => {
                         message: 'Something is not right',
                         user: user});
                 }
-                req.login(user, {session:false},(error)
-                =>{ if(error){res.send(error)}
+                req.login(user, {session: false},(error) =>
+                {if (error){res.send(error)}
                 let token = generateJWTToken(user.toJSON());
-
                 return res.json({user,token});
                 })    
             })(req,res);
@@ -38,7 +37,25 @@ module.exports = (router) => {
 
 
 
-
+    module.exports = (router) => {
+        router.post('/login', (req, res) => {
+          passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+              return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+              });
+            }
+            req.login(user, { session: false }, (error) => {
+              if (error) {
+                res.send(error);
+              }
+              let token = generateJWTToken(user.toJSON());
+              return res.json({ user, token });
+            });
+          })(req, res);
+        });
+      }
 
 
       
