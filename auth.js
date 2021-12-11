e93e6a3f2da11facff80b424e1f5314b1a72b45d
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-12-11 17:49:47 -0500

    Adding code to initialize passport routes

--- auth.js
+++ auth.js
@@ -5,18 +5,17 @@ const jwtSecret = 'your_jwt_secret'; //Check key used in JWT strategy for consis
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
  
-require('./passport');//What is my local passport file?
+require('./passport.js');//What is my local passport file?
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret,{
         subject: user.Username, //the encoded username
         expiresIn: '7d', //when the token expires
         algorithm: 'HS256'});//encoding JWT
-    
-}
+    }
 
 //What happens after login? 
-
+/*
 module.exports = (router) => {
     router.post('/login', (req,res) =>{
         passport.authenticate('local',{session: false},
@@ -25,19 +24,20 @@ module.exports = (router) => {
                     return res.status(400).json({
                         message: 'Something is not right',
                         user: user});
+    }            }
+                req.login (user, { session: false },(error) => {
+                if (error){
+                    res.send(error)
                 }
-                req.login(user, {session: false},(error) =>
-                {if (error){res.send(error)}
                 let token = generateJWTToken(user.toJSON());
                 return res.json({user,token});
                 })    
             })(req,res);
         });
-    }
+    }*/
 
-
-
-    module.exports = (router) => {
+   module.exports = (router) => {
+    router.use(passport.initialize());
         router.post('/login', (req, res) => {
           passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
@@ -55,8 +55,6 @@ module.exports = (router) => {
             });
           })(req, res);
         });
-      }
-
-
       
-
+    }
+    
