bec1c48a9e3c887d874a56a4a7fe48fcc3258283
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-12-12 11:41:36 -0500

    Importing Bcrypt to models.js

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 // This is where the Mongoose models will live. //
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 //Establishing schema for the movie database.
 
 let movieSchema = mongoose.Schema({
@@ -30,7 +32,7 @@ let userSchema = mongoose.Schema({
     Email: {type: String, required: true},
     Birthday: Date, 
     FavoriteMovies: 
-    [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+    [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
 //Assigning variables Movie/User to their respective schemas. 
