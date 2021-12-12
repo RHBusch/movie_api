a6e4ca68dfc8ee9ae135e9fbd2476337c9eb2ee2
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-12-12 18:59:31 -0500

    Extending validation to include when users update user information

--- index.js
+++ index.js
@@ -132,11 +132,23 @@ app.get('/movies', passport.authenticate('jwt',{session: false}),
   //Routing the update username request. (WORKS CORRECTLY)
 
   app.put('/users/:Username',passport.authenticate('jwt',{session: false}),
+  [
+    check('Username','Username is required').isLength({min:5}),
+    check('Username', 'Username cannot contain non alphanumeric characters.').isAlphanumeric(),
+    check('Password','Password is required.').not().isEmpty(),
+    check('Email','Email does not appear to be valid.').isEmail()
+
+],
   (req,res) =>{
+    let errors = validationResult(req);
+    if(!errors.isEmpty()){return res.status(422).json({errors: errors.array()
+ })
+     }
+  let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate({Username: req.params.Username},{$set:
         {
             Username: req.body.Username,
-            Password: req.body.Password, 
+            Password: hashedPassword, 
             Email: req.body.Email, 
             Birthday: req.body.Birthday
         }
