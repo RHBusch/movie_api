9d5a33ff9f8a04108da77435e872f92db5dd9d00
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-12-12 11:34:28 -0500

    implementing CORS in index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const app = express ();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cors = require('cors');
+app.use(cors());
+
+
+
 let auth = require('./auth.js')(app);
 const passport = require('passport');
 require('./passport.js');
